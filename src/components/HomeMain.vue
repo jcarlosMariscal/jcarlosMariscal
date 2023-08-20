5561ca08d1b79736663be87f9444f77e3399732d
JCarlosrm <juanc.ramirezm16@gmail.com>
2023-08-20 15:10:04 -0600

    Establecer colores | Efectos a botones

--- src/components/HomeMain.vue
+++ src/components/HomeMain.vue
@@ -15,8 +15,10 @@
           tecnologías en el desarrollo.
         </p>
         <div class="resume__btns">
-          <button type="button" class="btn btn-contact">Contáctame</button>
-          <button type="button" class="btn btn-cv">Curriculum</button>
+          <button type="button" class="btn btn-contact btn-anim">
+            Contáctame
+          </button>
+          <button type="button" class="btn btn-cv btn-anim">Curriculum</button>
         </div>
       </div>
     </div>
@@ -57,7 +59,11 @@
   line-height: 3.5rem;
 }
 .resume__title .title-name {
-  color: blue;
+  /* color: blue; */
+  font-weight: bold !important;
+  background: var(--gradient-background);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 .resume__description {
   font-size: 1rem;
@@ -70,9 +76,9 @@
   gap: 1rem;
 }
 .btn {
-  background: blue;
-  border: 1px solid blue !important;
-  color: var(--permanent-white);
+  background: transparent;
+  border: 1px solid var(--color-primary) !important;
+  color: var(--color-text);
   border: none;
   padding: 0.8rem 1.2rem;
   width: 8rem;
@@ -84,21 +90,38 @@
   font-size: 1rem;
   transition: all 0.2s ease-in;
   cursor: pointer;
+  background-image: linear-gradient(
+    to left,
+    transparent,
+    transparent 50%,
+    var(--color-primary) 50%,
+    var(--color-primary)
+  );
+  background-position: 100% 0;
+  background-size: 200% 100%;
+  transition: all 0.25s ease-in;
 }
 .btn:hover {
-  transition: all 0.2s ease-in;
-  background: transparent;
-  color: blue;
-}
-.btn-cv {
-  background: transparent;
-  border: 1px solid red !important;
-  color: red;
-}
-.btn-cv:hover {
-  background: red;
+  background-position: 0 0;
+  /* color: #333; */
   color: var(--permanent-white);
+  transition: all 0.2s ease-in;
 }
+
+.btn-cv {
+  border: 1px solid var(--color-secondary) !important;
+  background-image: linear-gradient(
+    to left,
+    transparent,
+    transparent 50%,
+    var(--color-secondary) 50%,
+    var(--color-secondary)
+  );
+}
+/* .btn-cv:hover {
+  background: var(--color-secondary);
+  color: var(--permanent-white);
+} */
 @media screen and (max-width: 1120px) {
   .resume__title {
     font-size: 2.3rem;
@@ -147,5 +170,109 @@
   .main__content-avatar {
     width: 80%;
   }
+}
+/* Blue Shadow */
+@-moz-keyframes blue {
+  0%,
+  100% {
+    -moz-box-shadow: 1px 0px 19px 4px rgba(0, 130, 196, 0.7),
+      inset 0px 0px 10px rgba(255, 255, 255, 0.5);
+    box-shadow: 1px 0px 19px 4px rgba(0, 130, 196, 0.7),
+      inset 0px 0px 10px rgba(255, 255, 255, 0.5);
+  }
+
+  50% {
+    -moz-box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+    box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+  }
+}
+
+@-webkit-keyframes blue {
+  0%,
+  100% {
+    -webkit-box-shadow: 1px 0px 19px 4px rgba(0, 130, 196, 0.7),
+      inset 0px 0px 10px rgba(255, 255, 255, 0.5);
+    box-shadow: 1px 0px 19px 4px rgba(0, 130, 196, 0.7),
+      inset 0px 0px 10px rgba(255, 255, 255, 0.5);
+  }
+
+  50% {
+    -webkit-box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+    box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+  }
+}
+
+@-o-keyframes blue {
+  0%,
+  100% {
+    box-shadow: 1px 0px 19px 4px rgba(0, 130, 196, 0.7),
+      inset 0px 0px 10px rgba(255, 255, 255, 0.5);
+  }
+
+  50% {
+    box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+  }
+}
+
+@keyframes primary {
+  0%,
+  100% {
+    box-shadow: 0px 0px 4px 1px var(--color-primary),
+      inset 0px 0px 1px var(--color-secondary);
+  }
+
+  50% {
+    box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+  }
+}
+@keyframes secondary {
+  0%,
+  100% {
+    box-shadow: 0px 0px 4px 1px var(--color-secondary),
+      inset 0px 0px 1px var(--color-primary);
+  }
+
+  50% {
+    box-shadow: 0px 0px 0px 0px rgba(0, 130, 196, 0),
+      inset 0px 0px 0px rgba(255, 255, 255, 0);
+  }
+}
+/* Button */
+.btn-anim {
+  /* text-align: center; */
+  /* padding: 10px 20px; */
+  /* text-decoration: none; */
+  /* color: #000; */
+  /* font-weight: bold; */
+  /* -webkit-border-radius: 10px; */
+  /* -moz-border-radius: 10px 0; */
+  /* border-radius: 10px 0; */
+  /* margin-right: 20px; */
+}
+
+/* Blue Background */
+.btn-anim {
+  /* text-shadow: 0px 1px 0px var(--color-primary); */
+  /* background-image: -webkit-linear-gradient(top, #87e0fd, #53cbf1); */
+  /* background-image: -moz-linear-gradient(top, #87e0fd, #53cbf1); */
+  /* background-image: -o-linear-gradient(top, #87e0fd, #53cbf1); */
+  /* background-image: linear-gradient(to bottom, #87e0fd, #53cbf1); */
+  -webkit-animation: primary 3s infinite;
+  -moz-animation: primary 3s infinite;
+  -o-animation: primary 3s infinite;
+  animation: primary 3s infinite;
+}
+.btn-anim.btn-cv {
+  /* text-shadow: 0px 1px 0px var(--color-secondary); */
+  -webkit-animation: secondary 3s infinite;
+  -moz-animation: secondary 3s infinite;
+  -o-animation: secondary 3s infinite;
+  animation: secondary 3s infinite;
 }
 </style>
--- src/components/NavBarMain.vue
+++ src/components/NavBarMain.vue
@@ -28,16 +28,16 @@
     <div class="navbar__menu" :class="{ 'mobile-active': mobileNavbar }">
       <ul class="navbar__menu-options">
         <li class="menu__option">
-          <a href="" class="item">Inicio</a>
+          <a href="" class="header-anim item draw">Inicio</a>
         </li>
         <li class="menu__option">
-          <a href="" class="item">Acerca de</a>
+          <a href="" class="header-anim item draw">Acerca de</a>
         </li>
         <li class="menu__option">
-          <a href="" class="item">Portafolio</a>
+          <a href="" class="header-anim item draw">Portafolio</a>
         </li>
         <li class="menu__option">
-          <a href="" class="item">Contacto</a>
+          <a href="" class="header-anim item draw">Contacto</a>
         </li>
       </ul>
     </div>
@@ -56,12 +56,20 @@ const changeTheme = () => {
     document.documentElement.style.setProperty("--color-text", "#ececee");
     document.documentElement.style.setProperty("--color-bg", "#141414");
     document.documentElement.style.setProperty(
+      "--navbar-bg",
+      "rgba(20, 20, 20, 0.96)"
+    );
+    document.documentElement.style.setProperty(
       "--background-image",
       `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30' patternTransform='scale(3) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(0, 0%, 8%, 1)'/><path d='M9 20.502h0A7.5 7.5 0 0 1 1.5 28 7.5 7.5 0 0 0-6 35.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1-21 35.5h0a7.5 7.5 0 0 1 7.5-7.5A7.5 7.5 0 0 0-6 20.502h0a7.5 7.5 0 1 1 15 0zm15 0h0A7.5 7.5 0 0 1 16.5 28a7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 20.502h0c0-2.072-.84-3.947-2.197-5.303A7.477 7.477 0 0 0 1.5 13.002a7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1-6 5.503h0a7.5 7.5 0 1 1 15 0h0c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196c2.07 0 3.945.84 5.303 2.197A7.471 7.471 0 0 1 24 20.502zm15 0h0A7.5 7.5 0 0 1 31.5 28a7.5 7.5 0 0 0-7.5 7.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 35.5h0a7.5 7.5 0 0 1 7.5-7.5 7.5 7.5 0 0 0 7.5-7.498h0a7.5 7.5 0 1 1 15 0zm0-30h0A7.5 7.5 0 0 1 31.5-2 7.5 7.5 0 0 0 24 5.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 5.5h0A7.5 7.5 0 0 1 16.5-2 7.5 7.5 0 0 0 24-9.498h0a7.5 7.5 0 1 1 15 0zm-22.5 37.5a7.5 7.5 0 0 0-7.5 7.5c0-2.072-.84-3.947-2.197-5.303A7.477 7.477 0 0 0 1.5 28.002 7.5 7.5 0 0 0 9 20.503c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196zm15-15a7.5 7.5 0 0 0-7.5 7.5c0-2.072-.84-3.947-2.197-5.303a7.477 7.477 0 0 0-5.303-2.197A7.5 7.5 0 0 0 24 5.503c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196z'  stroke-width='1.5' stroke='hsla(126, 23%, 49%, 0.01)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`
     );
   } else {
     document.documentElement.style.setProperty("--color-text", "#141414");
     document.documentElement.style.setProperty("--color-bg", "#ececee");
+    document.documentElement.style.setProperty(
+      "--navbar-bg",
+      "rgba(236, 236, 238, 0.96)"
+    );
     document.documentElement.style.setProperty(
       "--background-image",
       `url("data:image/svg+xml,<svg id='patternId' width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='30' height='30' patternTransform='scale(3) rotate(0)'><rect x='0' y='0' width='100%' height='100%' fill='hsla(240, 6%, 93%, 1)'/><path d='M9 20.502h0A7.5 7.5 0 0 1 1.5 28 7.5 7.5 0 0 0-6 35.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1-21 35.5h0a7.5 7.5 0 0 1 7.5-7.5A7.5 7.5 0 0 0-6 20.502h0a7.5 7.5 0 1 1 15 0zm15 0h0A7.5 7.5 0 0 1 16.5 28a7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 20.502h0c0-2.072-.84-3.947-2.197-5.303A7.477 7.477 0 0 0 1.5 13.002a7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1-6 5.503h0a7.5 7.5 0 1 1 15 0h0c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196c2.07 0 3.945.84 5.303 2.197A7.471 7.471 0 0 1 24 20.502zm15 0h0A7.5 7.5 0 0 1 31.5 28a7.5 7.5 0 0 0-7.5 7.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 35.5h0a7.5 7.5 0 0 1 7.5-7.5 7.5 7.5 0 0 0 7.5-7.498h0a7.5 7.5 0 1 1 15 0zm0-30h0A7.5 7.5 0 0 1 31.5-2 7.5 7.5 0 0 0 24 5.5h0a7.5 7.5 0 0 1-7.5 7.499 7.476 7.476 0 0 1-5.304-2.196A7.482 7.482 0 0 1 9 5.5h0A7.5 7.5 0 0 1 16.5-2 7.5 7.5 0 0 0 24-9.498h0a7.5 7.5 0 1 1 15 0zm-22.5 37.5a7.5 7.5 0 0 0-7.5 7.5c0-2.072-.84-3.947-2.197-5.303A7.477 7.477 0 0 0 1.5 28.002 7.5 7.5 0 0 0 9 20.503c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196zm15-15a7.5 7.5 0 0 0-7.5 7.5c0-2.072-.84-3.947-2.197-5.303a7.477 7.477 0 0 0-5.303-2.197A7.5 7.5 0 0 0 24 5.503c0 2.07.84 3.945 2.196 5.303a7.476 7.476 0 0 0 5.304 2.196z'  stroke-width='1.5' stroke='hsla(126, 23%, 49%, 0.02)' fill='none'/></pattern></defs><rect width='800%' height='800%' transform='translate(0,0)' fill='url(%23a)'/></svg>")`
@@ -95,8 +103,8 @@ const changeTheme = () => {
   align-items: center;
 }
 .navbar__resume-profile .profile-icon {
-  background: var(--color-text);
-  color: var(--color-bg) !important;
+  background: var(--gradient-background);
+  color: var(--permanent-white) !important;
   width: 2rem;
   height: 2rem;
   padding: 0.3rem;
@@ -132,14 +140,18 @@ const changeTheme = () => {
   padding: 0 0.3rem;
   display: flex;
   align-items: center;
-}
-.navbar__menu-options .menu__option .item {
   font-size: 1rem;
   line-height: 24px;
   transition: all 0.1s ease-in;
   padding: 0.5rem 0.2rem;
   color: var(--color-text);
   font-weight: 500 !important;
+  /* background-color: aqua; */
+  /* border: 1px solid red; */
+}
+.navbar__menu-options .menu__option .item:hover {
+  color: var(--color-primary);
+  /* border-bottom: 1px solid var(--color-primary); */
 }
 
 @media screen and (max-width: 800px) {
@@ -178,7 +190,7 @@ const changeTheme = () => {
   }
   .navbar__menu-options {
     flex-direction: column;
-    background: rgba(245, 245, 250, 0.96);
+    background: var(--navbar-bg);
     height: 100%;
     width: 15rem;
     padding-left: 0.5rem;
@@ -199,5 +211,63 @@ const changeTheme = () => {
     display: flex;
     justify-content: center;
   }
+}
+
+/* Source --: https://www.sliderrevolution.com/resources/css-border-animation/ ++ CSS Border transitions [Draw] ++ */
+.header-anim {
+  background: none;
+  border: 0;
+  box-sizing: border-box;
+  font-size: inherit;
+  font-weight: 700;
+  position: relative;
+  vertical-align: middle;
+  padding: 0.5rem 1rem !important;
+}
+.header-anim::before,
+.header-anim::after {
+  box-sizing: inherit;
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.draw {
+  transition: color 0.25s;
+}
+.draw::before,
+.draw::after {
+  border: 2px solid transparent;
+  width: 0;
+  height: 0;
+}
+.draw::before {
+  top: 0;
+  left: 0;
+}
+.draw::after {
+  bottom: 0;
+  right: 0;
+}
+.draw:hover {
+  color: var(--color-primary);
+}
+.draw:hover::before,
+.draw:hover::after {
+  width: 100%;
+  height: 100%;
+}
+.draw:hover::before {
+  border-bottom-color: var(--color-primary);
+  /* border-right-color: var(--color-primary); */
+  /* border-left-color: var(--color-primary); */
+  border-top-color: var(--color-primary);
+  transition: width 0.15s ease-out, height 0.25s ease-out 0.0001s;
+}
+.draw:hover::after {
+  /* border-bottom-color: var(--color-primary); */
+  /* border-left-color: var(--color-secondary); */
+  /* transition: border-color 0s ease-out 0.5s, width 0.15s ease-out 0.5s, */
+  /* height 0.25s ease-out 0.75s; */
 }
 </style>
