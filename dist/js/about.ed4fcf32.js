80b48c968a915a42fcddf6388c0490ed14023e4c
JCarlosrm <juanc.ramirezm16@gmail.com>
2023-08-29 22:10:04 -0600

    JC V1.0.1

--- dist/js/about.ed4fcf32.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkjcarlosmariscal"]=self["webpackChunkjcarlosmariscal"]||[]).push([[443],{4007:function(a,n,r){r.r(n),r.d(n,{default:function(){return f}});var s=r(3396);const c={class:"about"},u=(0,s._)("h1",null,"This is an about page",-1),t=[u];function e(a,n){return(0,s.wg)(),(0,s.iD)("div",c,t)}var i=r(89);const l={},o=(0,i.Z)(l,[["render",e]]);var f=o}}]);
-//# sourceMappingURL=about.ed4fcf32.js.map
